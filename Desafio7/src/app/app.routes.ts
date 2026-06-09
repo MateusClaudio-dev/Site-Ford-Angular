8d8fbd623241c260ca8d65ecc161af161da82c5a
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-09 14:45:21 -0300

    add config routes

--- Desafio7/src/app/app.routes.ts
+++ Desafio7/src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { Dashboard } from './components/dashboard/dashboard';
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'Login',
+        pathMatch: 'full'
+    },
+
+    { path: 'Login',    component: Login },
+    { path: 'Home',     component: Home },
+    { path: 'Dashboard',component: Dashboard }
+];
