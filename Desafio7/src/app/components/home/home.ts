363b30e467eee7b84fc6f0a2b87a205ab2ee8ce2
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-09 16:58:26 -0300

    Add structure home.htm

--- Desafio7/src/app/components/home/home.ts
+++ Desafio7/src/app/components/home/home.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  executarLogout() {
+    
+  }
+
+  openMenu(): void {
+    const header = document.querySelector('.home-header') as HTMLElement
+    const card = document.querySelector('.welcome-card') as HTMLElement
+    const iconMenu = document.querySelector('.icon-menu') as HTMLElement
+    const sideBar = document.querySelector('.side-bar') as HTMLElement
+      iconMenu.addEventListener('click', () => {
+      header.style.display = 'none'
+      card.style.display = 'none'
+      sideBar.style.display = 'block'
+      
+    })
+  }
+
+  outMenu(): void {
+    const header = document.querySelector('.home-header') as HTMLElement
+    const sideBar = document.querySelector('.side-bar') as HTMLElement
+    sideBar.addEventListener('click', () => {
+      // aqui display deve ser none para o side-bar, ou seja menu fechar
+      header.style.display = 'flex'
+      sideBar.style.display = 'none'
+    })
+  }
+
+  closeMenu() {
+
+  }
+}
