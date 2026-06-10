c98a838d70d2e6e25b6f8770cde859830fcf4bea
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-10 16:48:33 -0300

    side bar refactor

--- Desafio7/src/app/components/dashboard/dashboard.ts
+++ Desafio7/src/app/components/dashboard/dashboard.ts
@@ -14,18 +14,22 @@ export class Dashboard {
   }
 
   openMenu(): void {
+    const dashboard = document.querySelector('.cabecalho-dashboard') as HTMLElement
     const iconMenu = document.querySelector('.icon-menu') as HTMLElement
     const sideBar = document.querySelector('.side-bar') as HTMLElement
     iconMenu.addEventListener('click', function(){
+    dashboard.style.display = 'none'
     sideBar.style.display = 'block'
     })
 
   }
 
   closeMenu(): void {
+    const dashboard = document.querySelector('.cabecalho-dashboard') as HTMLElement
     const iconClose = document.querySelector('.icon-close') as HTMLElement
     const sideBar = document.querySelector('.side-bar') as HTMLElement
     iconClose.addEventListener('click', function(){
+     dashboard.style.display = 'flex'
     sideBar.style.display = 'none'
     })
   }
