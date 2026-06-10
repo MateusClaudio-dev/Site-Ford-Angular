babc47e82fff308fd9c292cbe65938787490fc8f
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-10 16:36:38 -0300

    add navBar component dashboard

--- Desafio7/src/app/components/dashboard/dashboard.ts
+++ Desafio7/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-import { Cabecalho } from '../cabecalho/cabecalho';
-import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Cabecalho],
+  imports: [RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard {
+    executarLogout() {
+    
+  }
+
+  openMenu(): void {
+    const iconMenu = document.querySelector('.icon-menu') as HTMLElement
+    const sideBar = document.querySelector('.side-bar') as HTMLElement
+    iconMenu.addEventListener('click', function(){
+    sideBar.style.display = 'block'
+    })
+
+  }
+
+  closeMenu(): void {
+    const iconClose = document.querySelector('.icon-close') as HTMLElement
+    const sideBar = document.querySelector('.side-bar') as HTMLElement
+    iconClose.addEventListener('click', function(){
+    sideBar.style.display = 'none'
+    })
+  }
+}
+
