98bdf48d1b0370ad8a9d0f23742af0f1d8d8c399
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-19 11:16:36 -0300

    test organizarCarrosPorModelo()

--- Desafio7/src/app/service/dashboard.service.ts
+++ Desafio7/src/app/service/dashboard.service.ts
@@ -1,5 +1,8 @@
+import { Veiculo } from './../models/veiculo.model';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Version } from '@angular/core';
+import { Veiculo } from '../models/veiculo.model';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +12,21 @@ export class DashboardService {
   private  API_URL = "http://localhost:3001/vehicles"
   constructor(private httpClient: HttpClient ) {}
 
-  obterCarros() {// ausencia de observables
-    return this.httpClient.get(this.API_URL);
+  obterCarros(): Observable<Veiculo[]> {// ausencia de observables
+    return this.httpClient.get<Veiculo[]>(this.API_URL);
   }
-  
-}
+
+
+
+// Um array com o id de cada carro e vinculado ao id as propiedades dos carros correspondente
+//  organizarCarrosPorModelo(): Observable<Map<string, Veiculo[]>> {
+//    return this.obterCarros().pipe(
+//      map((carros: Veiculo[]) => {
+//        const carroPorMarca = new Map<string, Veiculo[]();
+//
+//      })
+//    ) // TIPE = encadeia outros operadores (aplica trasnformações no retorno da api)
+//
+//    )
+//  }
+//}
