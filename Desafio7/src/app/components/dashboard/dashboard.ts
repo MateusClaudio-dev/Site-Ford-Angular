eb8d8f5ad34e3feb47194534ef5700f2430f1531
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-21 12:25:23 -0300

    git commit -m "feat(dashboard): implementar busca reativa por VIN e exibir telemetria"

--- Desafio7/src/app/components/dashboard/dashboard.ts
+++ Desafio7/src/app/components/dashboard/dashboard.ts
@@ -1,62 +1,94 @@
 import { DashboardService } from './../../service/dashboard.service';
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Veiculo, VeiculosAPI } from '../../models/veiculo.model';
-import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { TelemetriaVeiculo } from '../../models/veiculo.model';
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 
-// LOGICA DO MENU HAMBURGER
-export class Dashboard  {
-    executarLogout() {
-    
+export class Dashboard implements OnInit {
+
+  carros: Veiculo[] = []
+  carroSelecionado: Veiculo | null = null
+  dadosTelemetria: TelemetriaVeiculo | null = null
+
+  private buscadorVin$ = new Subject<string>();
+
+  // BLOCO DO CONSUMO DE API 
+  constructor(
+    private DashboardService: DashboardService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(){
+
+    // LÓGICA DO RETORNO DOS DADOS DA API
+    this.DashboardService.obterCarros().subscribe((resposta: VeiculosAPI) => {
+      this.carros = resposta.vehicles;
+      console.log(this.carros);
+      this.cdr.detectChanges();
+    });
+
+    // LÓGICA DO CAMPO DE BUSCA (VIN)
+    this.buscadorVin$.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((textoDoinput) => {
+        return this.DashboardService.obterDadosPorVin(textoDoinput)
+      })
+    ).subscribe((resultadoDaAPI) => {
+      this.dadosTelemetria = resultadoDaAPI;
+      this.cdr.detectChanges();
+    })
+  }
+
+  aoSelecionarCarro(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement; // 'target' aponta diretamente para o elemento que disparou o click
+    const idSelecionado = Number(selectElement.value);
+
+      this.carroSelecionado =  this.carros.find(carro => carro.id == idSelecionado) || null;
+      console.log(idSelecionado)
+  }
+
+  aoDigitarVin(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement) {
+      this.buscadorVin$.next(inputElement.value);
+    }
+  }
+
+// BLOCO DA LOGICA DO MENU HAMBURGUER
+  executarLogout() {
+    console.log('Sessão finalizada.');
   }
 
   openMenu(): void {
-    const dashboard = document.querySelector('.cabecalho-dashboard') as HTMLElement
-    const iconMenu = document.querySelector('.icon-menu') as HTMLElement
-    const sideBar = document.querySelector('.side-bar') as HTMLElement
-    iconMenu.addEventListener('click', function(){
-    dashboard.style.display = 'none'
-    sideBar.style.display = 'block'
-    })
-
+    const dashboard = document.querySelector('.cabecalho-dashboard') as HTMLElement;
+    const sideBar = document.querySelector('.side-bar') as HTMLElement;
+    
+    if (dashboard && sideBar) {
+      dashboard.style.display = 'none';
+      sideBar.style.display = 'block';
+    }
   }
 
   closeMenu(): void {
-    const dashboard = document.querySelector('.cabecalho-dashboard') as HTMLElement
-    const iconClose = document.querySelector('.icon-close') as HTMLElement
-    const sideBar = document.querySelector('.side-bar') as HTMLElement
-    iconClose.addEventListener('click', function(){
-     dashboard.style.display = 'flex'
-    sideBar.style.display = 'none'
-    })
-  }
-
-  carros: Veiculo[] = []
-
-  // BLOCO DO CONSUMO DE API 
-  constructor(private DashboardService: DashboardService) {}
-
-  ngOnInit(){
-    this.DashboardService.obterCarros().subscribe((resposta: VeiculosAPI) => {
-      this.carros = resposta.vehicles
-      console.log(this.carros)
-    });
-  }
-
-  @Input() vehicles: Veiculo = {
-    id: '1',
-    vehicle: "Testando",
-    volumetotal: 1,
-    connected: 2,
-    softwareUpdates: 3,
+    const dashboard = document.querySelector('.cabecalho-dashboard') as HTMLElement;
+    const sideBar = document.querySelector('.side-bar') as HTMLElement;
+    
+    if (dashboard && sideBar) {
+      dashboard.style.display = 'flex';
+      sideBar.style.display = 'none';
+    }
   }
 }
-
--- Desafio7/src/app/service/dashboard.service.ts
+++ Desafio7/src/app/service/dashboard.service.ts
@@ -1,7 +1,7 @@
-import { Veiculo, VeiculosAPI } from './../models/veiculo.model';
+import { VeiculosAPI, TelemetriaVeiculo } from './../models/veiculo.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,34 +9,14 @@ import { map, Observable } from 'rxjs';
 export class DashboardService {
 
   private  API_URL = "http://localhost:3001/vehicles"
+  private  DATA_URL = "http://localhost:3001/vehicleData"
   constructor(private httpClient: HttpClient ) {}
 
   obterCarros(): Observable<VeiculosAPI> {
     return this.httpClient.get<VeiculosAPI>(this.API_URL);
   }
 
-
-// Um array com o id de cada carro e vinculado ao id as propiedades dos carros correspondente
-//  organizarCarrosPorModelo(): Observable<Map<string, Veiculo[]>> {
-//    
-//    return this.obterCarros().pipe( // TIPE = encadeia outros operadores (aplica trasnformações no retorno da api)
-//      map((carros: Veiculo[]) => {
-//        const carrosPorMarca = new Map<string, Veiculo[]>();
-//
-//        carros.forEach((carros: Veiculo) => {
-//          const marcaId = typeof carros.id === 'string' ? carros.id : carros.id
-//
-//          if (marcaId) {
-//            if(!carrosPorMarca.has(marcaId)) {
-//              carrosPorMarca.set(marcaId, [])
-//            }
-//            carrosPorMarca.get(marcaId)?.push(carros)
-//          }
-//          
-//        })
-//        return carrosPorMarca
-//      })
-//    )
-//
-//  }
+  obterDadosPorVin(codigoDigitado: string): Observable<TelemetriaVeiculo> {
+    return this.httpClient.post<TelemetriaVeiculo>(this.DATA_URL, { vin: codigoDigitado })
+  }
 }
