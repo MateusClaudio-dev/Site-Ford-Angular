b80ec9c9db6aa779af6a8af26eefb86fa03dedb2
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-26 09:29:41 -0300

    fix(auth-guard.ts): corrige leitura do sessionStorage no authGuard

--- Desafio7/src/app/guards/auth-guard.ts
+++ Desafio7/src/app/guards/auth-guard.ts
@@ -4,7 +4,7 @@ import { inject } from '@angular/core';
 export const authGuard: CanActivateFn = (route, state) => { 
   const router = inject(Router);
   const logadoLocal = localStorage.getItem('usuarioLogado');
-  const logadoSession = localStorage.getItem('usuarioLogado');
+  const logadoSession = sessionStorage.getItem('usuarioLogado');
   if (logadoLocal === 'true' || logadoSession === 'true') {
     return true;
   } else {
